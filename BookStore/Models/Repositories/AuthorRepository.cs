1abb656ac038abe3ec3e771810ea8e1757d5c7bd
asmaahassnasr <asmaa14has@gmail.com>
2021-07-28 22:48:35 +0200

    Creating AuthorRepository and Impelement all methods of Interface

---
+++ BookStore/Models/Repositories/AuthorRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repositories
+{
+    public class AuthorRepository : IBookRepository<Author>
+    {
+        IList<Author> authors;
+        public AuthorRepository()
+        {
+            authors = new List<Author>() {
+                new Author{Id=1, FullName="Author 001"},
+                new Author{Id=2, FullName="Author 002"},
+                new Author{Id=3, FullName="Author 003"},
+                new Author{Id=4, FullName="Author 004"},
+                new Author{Id=5, FullName="Author 005"},
+                new Author{Id=6, FullName="Author 006"},
+            };
+        }
+        public void Add(Author entity)
+        {
+            authors.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var author = Find(id);
+            authors.Remove(author);
+        }
+
+        public Author Find(int id)
+        {
+            var author = authors.SingleOrDefault(a => a.Id == id);
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return authors;
+        }
+
+        public void Update(int id, Author entity)
+        {
+            var author = Find(id);
+            author.FullName = entity.FullName;
+        }
+    }
+}
+
+

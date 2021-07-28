7e06e15c22f1ab27552fb1c42126ffe9d5034513
asmaahassnasr <asmaa14has@gmail.com>
2021-07-28 23:01:45 +0200

    Creating Details method for speceific author

--- BookStore/Controllers/AuthorController.cs
+++ BookStore/Controllers/AuthorController.cs
@@ -27,7 +27,8 @@ namespace BookStore.Controllers
         // GET: AuthorController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var author = _authorRepository.Find(id);
+            return View(author);
         }
 
         // GET: AuthorController/Create

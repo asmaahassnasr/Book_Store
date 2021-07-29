029b53bd83e0b0faf76bc934aabdeb99def7a2d8
asmaahassnasr <asmaa14has@gmail.com>
2021-07-29 08:12:17 +0200

    apply authors to Books at BookRepo model

--- BookStore/Models/Repositories/BookRepository.cs
+++ BookStore/Models/Repositories/BookRepository.cs
@@ -11,12 +11,12 @@ namespace BookStore.Models.Repositories
         public BookRepository()
         {
             books = new List<Book>() {
-            new Book{Id=1, Title="Book 001" , Description="This is book 001" , Author= new Author()} ,
-            new Book{Id=2, Title="Book 002" , Description="This is book 002" , Author= new Author()} ,
-            new Book{Id=3, Title="Book 003" , Description="This is book 003" , Author= new Author()} ,
-            new Book{Id=4, Title="Book 004" , Description="This is book 004", Author= new Author() } ,
-            new Book{Id=5, Title="Book 005" , Description="This is book 005" , Author= new Author()} ,
-            new Book{Id=6, Title="Book 006" , Description="This is book 006" , Author= new Author()}
+            new Book{Id=1, Title="Book 001" , Description="This is book 001" , Author= new Author{ Id=1} } ,
+            new Book{Id=2, Title="Book 002" , Description="This is book 002" , Author= new Author{ Id=1} } ,
+            new Book{Id=3, Title="Book 003" , Description="This is book 003" , Author= new Author{ Id=3} } ,
+            new Book{Id=4, Title="Book 004" , Description="This is book 004", Author= new Author{ Id=2}  } ,
+            new Book{Id=5, Title="Book 005" , Description="This is book 005" , Author= new Author{ Id=3} } ,
+            new Book{Id=6, Title="Book 006" , Description="This is book 006" , Author= new Author{ Id=3} }
             };
         }
         public void Add(Book entity)

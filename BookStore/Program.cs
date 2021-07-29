9fb2d8fcd5cea4449eefbd42ea20cc379dc8caef
asmaahassnasr <asmaa14has@gmail.com>
2021-07-29 16:08:30 +0200

    Edtting program.cs for deployment on local DB

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using BookStore.Models;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore
 {
@@ -13,7 +16,16 @@ namespace BookStore
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var webHost = CreateHostBuilder(args).Build();
+            RunMigration(webHost);
+            webHost.Run();
+        }
+        public static void RunMigration(IHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope()) {
+                var db = scope.ServiceProvider.GetRequiredService<BookStoreDbContext>();
+                db.Database.Migrate();
+            };
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

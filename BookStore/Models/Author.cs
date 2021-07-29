51f79de188b791169b3dfc7ca9a0feb039fcfcc3
asmaahassnasr <asmaa14has@gmail.com>
2021-07-29 09:08:02 +0200

    Additn some validation attributes to the model

--- BookStore/Models/Author.cs
+++ BookStore/Models/Author.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace BookStore.Models
     public class Author
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =10)]
         public string FullName { get; set; }
 
     }
--- BookStore/ViewModels/BookAuthorViewModel.cs
+++ BookStore/ViewModels/BookAuthorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using BookStore.Models;
@@ -10,7 +11,12 @@ namespace BookStore.ViewModels
     {
         public int BookId { get; set; }
         public int  AuthorId { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [MinLength(5)]
         public string Title { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
         public string Description { get; set; }
         public List<Author> Authors { get; set; }
     }

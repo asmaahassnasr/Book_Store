fa165f8d685059b02c8429d8d7f345e4d9e10a4e
asmaahassnasr <asmaa14has@gmail.com>
2021-07-29 10:30:37 +0200

    BookRepository updates with some images and Update Method changes

--- BookStore/Models/Repositories/BookRepository.cs
+++ BookStore/Models/Repositories/BookRepository.cs
@@ -11,12 +11,12 @@ namespace BookStore.Models.Repositories
         public BookRepository()
         {
             books = new List<Book>() {
-            new Book{Id=1, Title="Book 001" , Description="This is book 001" , Author= new Author{ Id=1} } ,
-            new Book{Id=2, Title="Book 002" , Description="This is book 002" , Author= new Author{ Id=1} } ,
-            new Book{Id=3, Title="Book 003" , Description="This is book 003" , Author= new Author{ Id=3} } ,
-            new Book{Id=4, Title="Book 004" , Description="This is book 004", Author= new Author{ Id=2}  } ,
-            new Book{Id=5, Title="Book 005" , Description="This is book 005" , Author= new Author{ Id=3} } ,
-            new Book{Id=6, Title="Book 006" , Description="This is book 006" , Author= new Author{ Id=3} }
+            new Book{Id=1, Title="Book 001" , Description="This is book 001" , Author= new Author{ Id=1} , ImageUrl="1.jpg"} ,
+            new Book{Id=2, Title="Book 002" , Description="This is book 002" , Author= new Author{ Id=1}  , ImageUrl="3.jpg"} ,
+            new Book{Id=3, Title="Book 003" , Description="This is book 003" , Author= new Author{ Id=3}  , ImageUrl="4.jpg"} ,
+            new Book{Id=4, Title="Book 004" , Description="This is book 004", Author= new Author{ Id=2}  , ImageUrl="5.jpg"} ,
+            new Book{Id=5, Title="Book 005" , Description="This is book 005" , Author= new Author{ Id=3}  , ImageUrl="6.jpg"} ,
+            new Book{Id=6, Title="Book 006" , Description="This is book 006" , Author= new Author{ Id=3}  , ImageUrl="8.jpg"}
             };
         }
         public void Add(Book entity)
@@ -50,6 +50,7 @@ namespace BookStore.Models.Repositories
             book.Title = entity.Title;
             book.Description = entity.Description;
             book.Author = entity.Author;
+            book.ImageUrl = entity.ImageUrl;
         }
     }
 
